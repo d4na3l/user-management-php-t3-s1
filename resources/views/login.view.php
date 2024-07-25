c862eef5fef6309465ef4ceb42b07fccfaefb4d2
Jesus Vivas <jesusvivas018@gmail.com>
2024-07-24 22:27:46 -0400

    Plantilla/base de la estructura del front

---
+++ resources/views/login.view.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <title>Login</title>
+</head>
+<body>
+    <div class="login">
+        <form action="" method="post">
+            <div class="input">
+                <input type="email" name="email" id="email" placeholder="Correo">
+                <input type="password" name="password" id="password" placeholder="Contraseña">
+            </div>
+            <button type="submit">Ingresar</button>
+        </form>
+    </div>
+</body>
+</html>
